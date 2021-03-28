a649e90fff903ec05d4b12ec2a7c94dca3666495
author <email>
2021-03-28 16:39:59 +0300

    Initial Commit

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Olga Trofimova on 28.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
